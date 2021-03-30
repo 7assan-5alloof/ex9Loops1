c908f1d807cc9474ddf2208075661d6d1b1bb0f2
7assan-5alloof <hassankhalloof20@gmail.com>
2021-03-30 15:35:35 +0400

    Write Program

--- main.cpp
+++ main.cpp
@@ -4,6 +4,34 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    cout << "Press Ctrl-C to stop..." << endl;
+
+    while (true)
+    {
+        // Define needed variables
+        int sum = 0;
+        int cnt = 0;
+        char confirm;
+
+        // Use do-while as long as the user wants to enter tempratures
+        do
+        {
+            // Use temporary variable to store temperatures used in average and i
+            int input;
+
+            // Ask for tempereature and confirmation
+            cout << "Enter a temprature from Sharjah: ";
+            cin >> input;
+            cnt++;
+            sum += input;
+            cout << "Enter another temprature ([Y]es/[N]o)? ";
+            cin >> confirm;
+        } while ((confirm == 'y') || (confirm == 'Y'));
+
+        // Calculate and display average
+        int average = sum / cnt;
+        cout << "Based on your input, the average of temperatures in Sharjah is " << average << endl;
+    }
+
     return 0;
 }
